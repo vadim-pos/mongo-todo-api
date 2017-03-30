82e0e8c23857f148de9942d60bfd3be20d9b3f02
Vadim Postanovkin <v.postanovkin@gmail.com>
2017-03-30 10:38:38 +0300

    Add log functionality

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('./config/config');
 
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -7,6 +8,18 @@ const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
 
 const app = express();
+
+app.use((req, res, next) => {
+    const now = new Date().toString();
+    const logInfo = `${now}: ${req.method} ${req.url}`;
+
+    fs.appendFile('server.log', logInfo + '\n', err => {
+        if (err) { console.log('Unable to write log info'); }
+    });
+
+    console.log(logInfo);
+    next();
+});
 
 app.listen(3000, () => console.log(`Running on port ${process.env.PORT}`));
 
